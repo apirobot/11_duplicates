71f892659456bad44fcf2d82205d0c513dddaa8b
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-02-02 11:35:57 +0300

    Added solution and README update.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# Anti-Duplicator
+## Anti-Duplicator
 
-[TODO. There will be project description]
+Script takes path to a folder, checks all sub-folders, sub-sub-folders... and shows duplicates (files with the same name and size).
 
-# Project Goals
+### Usage
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+```
+usage: duplicates.py [-h] DIR
+
+Anti-Duplicator
+
+positional arguments:
+  DIR         path to a directory
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,51 @@
+import argparse
+import os.path
+from os import walk
+from collections import defaultdict
+
+
+def is_dir(path):
+    """Checks if ``path`` is an actual directory."""
+    if not os.path.isdir(path):
+        msg = '{0} is not a directory'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return path
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Anti-Duplicator')
+    parser.add_argument('directory', metavar='DIR', type=is_dir,
+                        help='path to a directory')
+    return parser
+
+
+def get_duplicates(directory):
+    fname_and_fsize_to_fpathes = defaultdict(list)
+    for (dirpath, dirnames, filenames) in walk(directory):
+        for filename in filenames:
+            fpath = os.path.abspath(os.path.join(dirpath, filename))
+            if os.path.isfile(fpath):
+                fsize = os.path.getsize(fpath)
+                fname_and_fsize_to_fpathes[filename+str(fsize)].append(fpath)
+
+    duplicates = [fpathes for fpathes in fname_and_fsize_to_fpathes.values()
+                          if len(fpathes) > 1]
+    return duplicates
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    duplicates = get_duplicates(args.directory)
+
+    for i, fpathes in enumerate(duplicates):
+        print('{:5} | '.format(i+1))
+        for fpath in fpathes:
+            print('{:5} | {}'.format('', fpath))
+        print('\n')
+
 
 if __name__ == '__main__':
-    pass
+    main()
